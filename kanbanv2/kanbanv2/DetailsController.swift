5d7d07c665d761f0fd12db383af89cd50b14ec2a
Ryan <ryan@Macbooks-MacBook-Pro.local>
2018-10-26 17:31:00 -0700

    Added DetailsController file

---
+++ kanbanv2/kanbanv2/DetailsController.swift
@@ -0,0 +1,17 @@
+//
+//  DetailsController.swift
+//  kanbanv2
+//
+//  Created by Ryan on 2018-10-26.
+//  Copyright © 2018 Ryan. All rights reserved.
+//
+
+import UIKit
+
+class DetailsController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+}
