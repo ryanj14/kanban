b7f64b1414b2ae47c8fe62a4044d08d168985a82
Ryan Joseph <a00995933@imac-10.ad.bcit.ca>
2018-10-27 14:01:04 -0700

    Landing page now displays content from data core

--- kanbanv2/kanbanv2/ProjectsController.swift
+++ kanbanv2/kanbanv2/ProjectsController.swift
@@ -9,16 +9,24 @@
 import UIKit
 import CoreData
 
-class ProjectsController: UITableViewController
+class ProjectsController: UITableViewController, NSFetchedResultsControllerDelegate
 {
+    private var managedObjectContext: NSManagedObjectContext? = nil
     
     override func viewDidLoad()
     {
         super.viewDidLoad()
         
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else
+        {
+            return
+        }
+        
+        managedObjectContext = appDelegate.persistentContainer.viewContext
+        
         // This creates an entry to the data core entity projects
         //createData()
-        retreieveData()
+        //retreieveData()
     }
     
     private func createData()
@@ -56,5 +64,83 @@ class ProjectsController: UITableViewController
         } catch {
             print("Failed")
         }
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return fetchedResultsController.sections?.count ?? 0
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        let sectionInfo = fetchedResultsController.sections![section]
+        let count = sectionInfo.numberOfObjects
+        
+        return count
+    }
+    
+    // This will only work for dynamic table view and not static
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TestingCell", for: indexPath)
+        let projects = fetchedResultsController.object(at: indexPath)
+        configureCell(cell, withProjects: projects)
+        return cell
+    }
+    
+    func configureCell(_ cell: UITableViewCell, withProjects projects: Projects)
+    {
+        cell.textLabel!.text = projects.name!.description
+    }
+    
+    // MARK: - Fetched results controller
+    var _fetchedResultsController: NSFetchedResultsController<Projects>? = nil
+    
+    var fetchedResultsController: NSFetchedResultsController<Projects> {
+        
+        
+        if _fetchedResultsController != nil {
+            return _fetchedResultsController!
+        }
+        
+        let fetchRequest: NSFetchRequest<Projects> = Projects.fetchRequest()
+        
+        // Set the batch size to a suitable number.
+        fetchRequest.fetchBatchSize = 20
+        
+        // Edit the sort key as appropriate.
+        let sortDescriptor = NSSortDescriptor(key: "name", ascending: true)
+        
+        fetchRequest.sortDescriptors = [sortDescriptor]
+        
+        // Edit the section name key path and cache name if appropriate.
+        // nil for section name key path means "no sections".
+        let aFetchedResultsController = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: self.managedObjectContext!, sectionNameKeyPath: nil, cacheName: nil)
+        aFetchedResultsController.delegate = self
+        _fetchedResultsController = aFetchedResultsController
+        
+        do {
+            try _fetchedResultsController!.performFetch()
+        } catch {
+            // Replace this implementation with code to handle the error appropriately.
+            // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+            let nserror = error as NSError
+            fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+        }
+        
+        return _fetchedResultsController!
+    }
+    
+    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.endUpdates()
+    }
+    
+}
+
+struct Project : Codable
+{
+    let name: String?
+    
+    private enum CodingKeys: String, CodingKey
+    {
+        case name
     }
 }
