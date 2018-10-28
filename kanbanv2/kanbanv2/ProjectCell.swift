653ddc3b7846133219b8deb8d915ea7b701a8697
Ryan Joseph <a00995933@imac-10.ad.bcit.ca>
2018-10-28 14:55:29 -0700

    Added ProjectTableViewCell file

---
+++ kanbanv2/kanbanv2/ProjectCell.swift
@@ -0,0 +1,15 @@
+//
+//  ProjectCell.swift
+//  kanbanv2
+//
+//  Created by Ryan Joseph on 2018-10-28.
+//  Copyright © 2018 Ryan. All rights reserved.
+//
+
+class ProjectCell: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+}
---
+++ kanbanv2/kanbanv2/ProjectCellTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ProjectCellTableViewCell.swift
+//  kanbanv2
+//
+//  Created by Ryan Joseph on 2018-10-28.
+//  Copyright © 2018 Ryan. All rights reserved.
+//
+
+import UIKit
+
+class ProjectCellTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
