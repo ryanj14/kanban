bbbebe774e63b0b42315cb2c1684efee2dff116f
Ryan <josephmryan14@gmail.com>
2018-11-07 15:14:01 -0800

    Added delete button subclass file

---
+++ kanbanv2/kanbanv2/DeleteButtonSubClass.swift
@@ -0,0 +1,21 @@
+//
+//  DeleteButtonSubClass.swift
+//  kanbanv2
+//
+//  Created by Ryan on 2018-11-07.
+//  Copyright © 2018 Ryan. All rights reserved.
+//
+
+import UIKit
+
+class DeleteButtonSubClass: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
