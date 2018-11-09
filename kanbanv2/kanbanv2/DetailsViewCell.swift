e7e89130d891aa00faef566a519eff901c9669ff
Ryan <josephmryan14@gmail.com>
2018-11-08 21:24:09 -0800

    Added DetailsViewCell file

---
+++ kanbanv2/kanbanv2/DetailsViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  DetailsViewCell.swift
+//  kanbanv2
+//
+//  Created by Ryan on 2018-11-08.
+//  Copyright © 2018 Ryan. All rights reserved.
+//
+
+import UIKit
+
+class DetailsViewCell: UITableViewCell {
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
